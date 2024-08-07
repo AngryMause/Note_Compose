2d1f94cea5a50ab62f179b05bd2808070a6a7776
angrymause <chostblade@gmail.com>
2024-08-08 01:13:20 +0300

    change bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.angry_mouse.freefireth"
+        applicationId = "com.angry_mouse.note"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
